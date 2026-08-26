aa106343f38daa1a28cbe9d01d1a264fb391946d
spykiller103 <nbje74801@edu.ucl.dk>
2026-08-26 14:04:55 +0200

    Made a new project with MVC (template)

---
+++ DiveDeep/DiveDeep/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using DiveDeep.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DiveDeep.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DiveDeep/DiveDeep/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DiveDeep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
