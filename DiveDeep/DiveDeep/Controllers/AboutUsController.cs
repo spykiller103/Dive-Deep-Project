a82c63a7e90433d97b842f3426e590ee5b9cbd51
Rasmus <rasmuus15@gmail.com>
2026-08-28 14:17:43 +0200

    Made the AboutUs Page

---
+++ DiveDeep/DiveDeep/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiveDeep.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
