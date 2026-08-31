238e0e763bc930f5e95c6add035c104c312f0521
Filip Meldal <fime77291.edu.ucl.dk>
2026-08-31 20:58:15 +0200

    Made equipment-site with modelbinding through perstistence classes (in-memory)

---
+++ DiveDeep/DiveDeep/Controllers/EquipmentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DiveDeep.Persistence;
+
+namespace DiveDeep.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var equipment = EquipmentRepository.GetAll();
+            return View(equipment);
+        }
+    }
+}
---
+++ DiveDeep/DiveDeep/Persistence/EquipmentRepository.cs
@@ -0,0 +1,36 @@
+﻿using DiveDeep.Models;
+
+namespace DiveDeep.Persistence
+{
+    public static class EquipmentRepository
+    {
+        private static List<Equipment> _equipment = new List<Equipment>
+        {
+
+            new Equipment
+            {
+                Category = "Beklædning",
+                Title = "Dykkermaske",
+                Description = "Silikone-maske med enkelt eller dobbelt linse. S/M/L",
+                Price = 95
+            },
+
+            new Equipment
+            {
+                Category = "Beklædning",
+                Title = "Finner",
+                Description = "Åbenhæls og lukkethæls finner. Størrelse 38-47",
+                Price = 75
+            }
+        };
+
+        public static List<Equipment> GetAll()
+        {
+            return _equipment;
+        }
+
+    }
+
+
+
+}
