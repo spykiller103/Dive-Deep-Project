eb44ee00872ff248873f9d66a57ad4456fd2a18f
Filip Meldal <filipmeldal@hotmail.com>
2026-09-04 12:03:08 +0200

    Added equipments

--- DiveDeep/DiveDeep/Models/Equipment.cs
+++ DiveDeep/DiveDeep/Models/Equipment.cs
@@ -2,10 +2,13 @@
 {
     public class Equipment
     {
-        public string? Category { get; set; }
-        public string? Title { get; set; }
-        public string? Description { get; set; }
-        public int? Price { get; set; }
+        public string ImageID { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+
+        
     }
 
 
--- DiveDeep/DiveDeep/Persistence/EquipmentRepository.cs
+++ DiveDeep/DiveDeep/Persistence/EquipmentRepository.cs
@@ -9,19 +9,283 @@ namespace DiveDeep.Persistence
 
             new Equipment
             {
-                Category = "Beklædning",
-                Title = "Dykkermaske",
-                Description = "Silikone-maske med enkelt eller dobbelt linse. S/M/L",
+                ImageID = "/Content/Images/Equipment/BCD/NavigatorLite.png",
+                Category = "BCD",
+                Title = "Scubapro Navigator Lite BCD",
+                Description = "TEMP",
                 Price = 95
             },
 
             new Equipment
             {
-                Category = "Beklædning",
-                Title = "Finner",
-                Description = "Åbenhæls og lukkethæls finner. Størrelse 38-47",
+                ImageID = "/Content/Images/Equipment/BCD/GlideBCD.png",
+                Category = "BCD",
+                Title = "Scubapro BCD Glide",
+                Description = "TEMP",
+                Price = 95
+            },
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/HydrosPro.png",
+                Category = "BCD",
+                Title = "Scubapro BCD Hydros Pro",
+                Description = "TEMP",
+                Price = 95
+            },
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/Modular.png",
+                Category = "BCD",
+                Title = "Seac BCD Modular",
+                Description = "TEMP",
+                Price = 95
+            },
+
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/Divingsuits/Definition.png",
+                Category = "Dykkerdragt",
+                Title = "Scubapro Definition",
+                Description = "3 mm",
+                Price = 95
+            },
+
+
+            new Equipment
+            {
+                ImageID = "/Content/Images/Divingsuits/BCD/Definition.png",
+                Category = "Dykkerdragt",
+                Title = "Scubapro Definition",
+                Description = "5 mm",
+                Price = 95
+            },
+             new Equipment
+            {
+                ImageID = "/Content/Images/Divingsuits/BCD/Definition.png",
+                Category = "Dykkerdragt",
+                Title = "Scubapro Definition",
+                Description = "7 mm",
+                Price = 95
+            },
+
+              new Equipment
+            {
+                ImageID = "/Content/Images/Divingsuits/BCD/W5.png",
+                Category = "Dykkerdragt",
+                Title = "Waterproof W5",
+                Description = "3.5 mm",
+                Price = 95
+            },
+
+               new Equipment
+            {
+                ImageID = "/Content/Images/Divingsuits/BCD/ProteusF.png",
+                Category = "Dykkerdragt",
+                Title = "Fourth Element Proteus",
+                Description = "5 mm",
+                Price = 95
+            },
+
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/Exodry4.png",
+                Category = "Dykkerdragt",
+                Title = "Scubapro Exodry 4.0",
+                Description = "N/A",
+                Price = 95
+            },
+             new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/D7Evo.png",
+                Category = "Dykkerdragt",
+                Title = "Waterproof D7 Evo",
+                Description = "N/A",
+                Price = 95
+            },
+
+              new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/ELitePlus.png",
+                Category = "Dykkerdragt",
+                Title = "Santi E.Lite Plus",
+                Description = "N/A",
+                Price = 95
+            },
+
+             new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/Tank.png",
+                Category = "Tanke",
+                Title = "Scubapro 5 liter",
+                Description = "N/A",
+                Price = 150
+            },
+              new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/Tank.png",
+                Category = "Tanke",
+                Title = "Scubapro 10 liter",
+                Description = "N/A",
+                Price = 160
+            },
+             new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/Tank.png",
+                Category = "Tanke",
+                Title = "Scubapro 12 liter",
+                Description = "N/A",
+                Price = 170
+            },
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/Tank.png",
+                Category = "Tanke",
+                Title = "Scubapro 15 liter",
+                Description = "N/A",
+                Price = 180
+            },
+
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/MK25EVO.png",
+                Category = "Regulatorsæt",
+                Title = "Scubapro Octopus R105/MK25EVO/S600",
+                Description = "N/A",
+                Price = 125
+            },
+               new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/MK17.png",
+                Category = "Regulatorsæt",
+                Title = "Scubapro Octopus R095/MK17EVO/C370",
+                Description = "N/A",
+                Price = 100
+            },
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/MK25EVObt.png",
+                Category = "Regulatorsæt",
+                Title = "Scubapro Octopus S270/MK25EVO BT/A700 Carbon BT",
+                Description = "N/A",
+                Price = 150
+            },
+
+             new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/Ghost.png",
+                Category = "Maske/Snorkel",
+                Title = "Scubapro Ghost",
+                Description = "N/A",
+                Price = 50
+            },
+                new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/DMask.png",
+                Category = "Maske/Snorkel",
+                Title = "Scubapro D-Mask",
+                Description = "N/A",
+                Price = 60
+            },
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/SpectraMini.png",
+                Category = "Maske/Snorkel",
+                Title = "Scubapro Spectra Mini",
+                Description = "N/A",
+                Price = 50
+            },
+             new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/CrystalVu.png",
+                Category = "Maske/Snorkel",
+                Title = "Scubapro Crystal VU",
+                Description = "N/A",
                 Price = 75
-            }
+            },
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/Scout.png",
+                Category = "Maske/Snorkel",
+                Title = "Fourth Element Scout Kontrast",
+                Description = "N/A",
+                Price = 75
+            },
+              new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/ScoutEnchance.png",
+                Category = "Maske/Snorkel",
+                Title = "Fourth Element Scout Enchance",
+                Description = "N/A",
+                Price = 75
+            },
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/Element.png",
+                Category = "Maske/Snorkel",
+                Title = "Tusa Element",
+                Description = "N/A",
+                Price = 75
+            },
+
+              new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/JetFin.png",
+                Category = "Finner",
+                Title = "Scubapro Jet Fin",
+                Description = "N/A",
+                Price = 50
+            },
+                 new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/TravelFins.png",
+                Category = "Finner",
+                Title = "Scubapro GO travel",
+                Description = "N/A",
+                Price = 50
+            },
+               new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/SeawingSupernova.png",
+                Category = "Finner",
+                Title = "Scubapro Seawing Supernova",
+                Description = "N/A",
+                Price = 60
+            },
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/Propulsion.png",
+                Category = "Finner",
+                Title = "Seac Propulsion",
+                Description = "N/A",
+                Price = 50
+            },
+               new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/ALA.png",
+                Category = "Finner",
+                Title = "Seac ALA",
+                Description = "N/A",
+                Price = 50
+            },
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/TechFins.png",
+                Category = "Finner",
+                Title = "Fourth Element Tech",
+                Description = "N/A",
+                Price = 75
+            },
+            new Equipment
+            {
+                ImageID = "/Content/Images/Equipment/BCD/RecFins.png",
+                Category = "Finner",
+                Title = "Fourth Element Rec Fin",
+                Description = "N/A",
+                Price = 80
+            },
+
+
+
         };
 
         public static List<Equipment> GetAll()
