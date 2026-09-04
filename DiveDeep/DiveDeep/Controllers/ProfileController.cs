3cde7a32e9bed85e78ef6c4194ee0a8db503c006
spykiller103 <nbje74801@edu.ucl.dk>
2026-09-04 12:10:19 +0200

    Made a controller and the profilepage

---
+++ DiveDeep/DiveDeep/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiveDeep.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
