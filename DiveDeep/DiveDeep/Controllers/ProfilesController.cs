925ebb0c3d78d2fbdae529494ff26b938feb2442
Rasmus <rasmuus15@gmail.com>
2026-09-08 09:40:37 +0200

    Made the Login and Signup pages

---
+++ DiveDeep/DiveDeep/Controllers/ProfilesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiveDeep.Controllers
+{
+    public class ProfilesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
