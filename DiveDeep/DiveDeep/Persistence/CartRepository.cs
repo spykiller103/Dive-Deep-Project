bd465068c88d8b9804fdb83ab0a735f7a6a65d49
Jeppe Helsted <jeppe@helsted.com>
2026-09-08 10:07:30 +0200

    Quick fix to cart repo and viewmodel

--- DiveDeep/DiveDeep/Persistence/CartRepository.cs
+++ DiveDeep/DiveDeep/Persistence/CartRepository.cs
@@ -3,12 +3,7 @@ namespace DiveDeep.Persistence
 {
     public class CartRepository
     {
-        public int Id { get; set; }
-        public string ImageID { get; set; }
-        public string Category { get; set; }
-        public string Title { get; set; }
      
-        public int Price { get; set; }
     }
 }
 
