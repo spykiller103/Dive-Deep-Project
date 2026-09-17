a2fcca7dfd2b75eace0cfcdad566ec7b26da906a
Rasmus <rasmuus15@gmail.com>
2026-09-17 13:30:18 +0200

    made cahnges to login details and made it fit the other sites

--- DiveDeep/DiveDeep/Data/ApplicationUser.cs
+++ DiveDeep/DiveDeep/Data/ApplicationUser.cs
@@ -4,6 +4,9 @@ namespace DiveDeep.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
     public int? ActiveRents { get; set; }
     public int? CompletedRents { get; set; }
     public int? Rents
