82dc75f274a382f71f408d9f867944870a1333aa
Jeppe Helsted <jeppe@helsted.com>
2026-09-24 14:26:24 +0200

    Fixed the empty cart feature so now you only delete all items for the person logged in

--- DiveDeep/DiveDeep/Controllers/CartsController.cs
+++ DiveDeep/DiveDeep/Controllers/CartsController.cs
@@ -5,6 +5,7 @@ using DiveDeep.Service;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DiveDeep.Controllers
 {
@@ -13,27 +14,35 @@ namespace DiveDeep.Controllers
     {
         private readonly CartService _cartService;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly DiveDeepContext _context;
         private readonly IBookingRepository _bookingRepository;
 
-        public CartsController(CartService cartService, IBookingRepository bookingRepository, UserManager<ApplicationUser> userManager)
+        public CartsController(CartService cartService, IBookingRepository bookingRepository, UserManager<ApplicationUser> userManager, DiveDeepContext context)
         {
             _cartService = cartService;
             _userManager = userManager;
+            _context = context;
             _bookingRepository = bookingRepository;
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult EmptyCart()
+        public async Task<IActionResult> EmptyCart()
         {
-            // Delete all cart items that are not part of a booking
+            // Delete all cart items that are not part of a booking and only for the logged in user
             List<CartItem> cartItems = _cartService.GetAll()
                 .Where(c => c.BookingId == null)
                 .ToList();
-
-            foreach (var item in cartItems)
+            ApplicationUser user = await _userManager.GetUserAsync(User);
+           if(user != null)
             {
-                _cartService.Delete(item.CartItemId);
+               var userCartItems = await _context.CartItems
+                        .Where(c => c.ApplicationUserId == user.Id) 
+                        .ToListAsync();
+                _context.CartItems.RemoveRange(userCartItems); //RemoveRange tracks items in the deleted state until DBContext.SaveChanges() is called
+
+                await _context.SaveChangesAsync();
+               
             }
 
             return RedirectToAction("Index");
